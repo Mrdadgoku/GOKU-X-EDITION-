c5aadc58aab82b60b8b68d09effd8e2157e78e84
Mrdadgoku <dr084891@gmail.com>
2024-06-10 17:28:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
-OWNER_ID = int(getenv("OWNER_ID", "6462365139"))
+OWNER_ID = int(getenv("OWNER_ID", "6462365138"))
 
 BOT_USERNAME = getenv("BOT_USERNAME" , "NezukoxAssistant_bot")
 
